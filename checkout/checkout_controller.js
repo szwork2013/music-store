b9d5b6afcc80853906c4bc3792a7bde71842686d
avrani <avranitube@gmail.com>
2015-08-06 21:27:57 +0300

    shimon

--- checkout/checkout_controller.js
+++ checkout/checkout_controller.js
@@ -6,14 +6,24 @@ app.controller( 'checkOutController', function($scope,$route,$location,$routePar
 	$scope.payment=false;
 	$scope.order=false;
 	
+	$scope.x1="black";
+	$scope.x2="red";
+	$scope.x3="red";
 
 		
 	$scope.billingFunc=function(){
 		$scope.billing=false;
 		$scope.payment=true;
+		$scope.x1="red";
+		$scope.x2="black";
 	}
-
 	
+	$scope.paymentFunc=function(){
+		$scope.order=true;
+		$scope.payment=false;
+		$scope.x2="red";
+		$scope.x3="black";
+	}
 	
 	
 }); 
