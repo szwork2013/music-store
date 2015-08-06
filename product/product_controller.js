cf18fd6a74dfcfdd8d169bdc799f44f9ee2db55b
Elad <noyelad@gmail.com>
2015-08-06 23:43:49 +0300

    add service to my cart localstorage

--- product/product_controller.js
+++ product/product_controller.js
@@ -1,13 +1,13 @@
 
 
-app.controller( 'productController', function($scope,$location,$routeParams,productFactory) {
+app.controller( 'productController', function($scope,$location,$routeParams,productFactory,productService) {
 	
 	$scope.Qty=0;
 	$scope.heart='emptyHeart';
 	$scope.album={};
 	$scope.id;
 	$scope.disableHeartBtn=false;
-	$scope.localStorageName;
+
 
 	$scope.countQty=function(upOrDown){
 		if(upOrDown=='up') {
@@ -30,69 +30,42 @@ app.controller( 'productController', function($scope,$location,$routeParams,prod
 
 	//////////////////////////   Local Storage manage    /////////////////////////////////
 
-	$scope.getData=function(){
-		var localData=localStorage.getItem($scope.localStorageName);
-		if(localData==null){
-			$scope.localData=[];
-		}
-		else{
-			$scope.localData= angular.fromJson(localData);
-		}
-	}
-
-	$scope.mergeData=function(){
-		var newData=$scope.album;
-		$scope.localData.push(newData);
-		$scope.updateData();
-	}
-
-	$scope.updateData=function(){
-		localStorage.setItem($scope.localStorageName, angular.toJson($scope.localData) );
-		$scope.heart='fullHeart';
-		$scope.disableHeartBtn=true;
-	}
-
-
 
 	$scope.addTo=function(addTo){
 		switch(addTo) {
 			case ('wish'):
 				if($scope.disableHeartBtn==false) {
-					$scope.localStorageName='MyWishList';
-					$scope.getData();
-					$scope.mergeData();
+					var localStorageName='MyWishList';
+					productService.mergeData($scope.album , localStorageName);
+					$scope.heart = 'fullHeart';
+					$scope.disableHeartBtn=true;
 				}
 				else{
 					alert('This product already in your wishlist.');
 				}
 			case('cart'):
-				$scope.localStorageName='MyCart';
-				$scope.getData();
-				$scope.mergeData();
+				var localStorageName='MyCart';
+				productService.mergeData($scope.album,localStorageName);
 		}
 
 	}
 
 
 	$scope.checkIfInWishlist=function(){
-		$scope.localStorageName='MyWishList';
-		$scope.getData();
-		var wishlist = $scope.localData;
-		for (var key in wishlist) {
-			if (wishlist[key].album_id == $routeParams.id) {
+		var isInWishlist=productService.checkIfInWishlist($routeParams.id);
+			if (isInWishlist) {
 				$scope.heart = 'fullHeart';
 				$scope.disableHeartBtn=true;
 			}
-		}
 	}
 
+	
 	/////////////////////////////////////////////////////////////////////////////////////////
 
 
 
 	$scope.getProducts();
 	$scope.checkIfInWishlist();
-
 
 
 }); //close productController
