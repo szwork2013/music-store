9a66f2ee6931a46ec91d59b3615ec3ded8dd1f3f
Elad <noyelad@gmail.com>
2015-08-07 11:33:08 +0300

    styleing css

--- home/home_controller.js
+++ home/home_controller.js
@@ -29,6 +29,7 @@ app.controller( 'homeController', function($scope,$location,$routeParams,HomeFac
     $scope.getCategories=function(){
         HomeFactory.getCategories().
             success(function (categories) {
+                console.log(categories);
                 $scope.categories=categories;
             });
     }
