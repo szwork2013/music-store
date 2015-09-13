2244e394ea1de282510e174a541a67c1fc76dcfb
avrani <avranitube@gmail.com>
2015-09-13 12:28:57 +0300

    goodForUpload

--- php/album/album_view.php
+++ php/album/album_view.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once(dirname(__FILE__) . '/album_Controller.php');
+include_once(dirname(__FILE__) . '/album_controller.php');
 include_once(dirname(__FILE__) . '/../../lib/Slim/Slim.php');
 
 \Slim\Slim::registerAutoloader();
